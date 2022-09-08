2dc6eb4e54697238d83a5a8fd75e2f3e25f4d737
ljiredale <ljiredale@student.nafcs.org>
2022-09-07 21:59:19 -0400

    Yoga Classes Assignment

--- main.py
+++ main.py
@@ -1,36 +1,26 @@
-def orderReq():
-    for i in range(1):
-        orderNum = int(input("Please input your order number: "))
-        customerName = input("Please input your name: ")
-        woodType = int(input("Enter wood type 1 for oak wood 2 for pine wood: "))
-        numberOfCharacters = int(input("Please input number of characters: "))
-        colWhite = int(input("Enter number of white colored characters: "))
-        colBlack = int(input("Enter number of black colored characters: "))
-        colGold = int(input("Enter number of gold colored characters: "))
-    runCalc(orderNum, customerName, woodType, colWhite, colBlack, colGold)
+classes = [
+        "Yoga 1", "Yoga 2", "Children's Yoga", "Prenatal Yoga", "Senior Yoga"
+    ]
+classcnt = [0, 0, 0, 0, 0]
 
-def runCalc(oNum, cName, wType, cWhite, cBlack, cGold):
-    charge = 30
-    if wType == 1:
-        charge += 15
-    if cWhite + cBlack + cGold > 6:
-        charge += (((cWhite + cBlack + cGold)-6)*3)
-    if cGold > 0:
-        charge += 12
-    if cGold > 10 and wType == 2:
-        print("Pine wood with more than 10 gold characters")
-        print("Order number is " + str(oNum))
-        print("Customer's name is " + cName)
-        print("Number of characters is " + str((cWhite + cGold + cBlack)))    
-        print(charge)
-    if cWhite == 5 and wType == 1:
-        print("Oak wood with more than 5 white characters")
-        print("Order number is " + str(oNum))
-        print("Customer's name is " + cName)
-        print("Number of characters is " + str((cWhite + cGold + cBlack)))    
-        print("Charge is " + str(charge))
+def printClasses():
+    print("\nEnter -1 to finish.")
+    for i in range(len(classes)):
+        print(str(i + 1) + ". " + classes[i])
 
+def main():
+    printClasses()
+    inputVal = int(input("Select a class:"))
+    if inputVal == -1:
+        finished()
+    elif inputVal == 0 or inputVal <= -2 or inputVal > 5:
+        print("Input a valid number.")
+        main()
+    else:
+        classcnt[inputVal-1] = classcnt[inputVal-1] + 1
+        main()
+def finished():
+    for i in range(len(classes)):
+        print("\n" + str(i + 1) + ". " + classes[i] + ": " + str(classcnt[i]))
 
-
-
-orderReq()
+main()
